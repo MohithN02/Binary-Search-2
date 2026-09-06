0457136ecf4d2773b7bf30308ec2a0363eff79a5
mohithchandranarra <narra.mohithchandra@gmail.com>
2026-09-06 10:54:23 -0400

    Binary Search - Find First and Last Element in Sorted Array

---
+++ FirstAndLastPositionInSortedArray.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    private int binarySearchLast(int[] nums, int target, int low, int high){
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                if(mid == high || nums[mid] < nums[mid+1]){
+                    return mid;
+                }else{
+                    low = mid + 1;
+                }
+            }else if(nums[mid] > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    private int binarySearchFirst(int[] nums, int target, int low, int high){
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                if(mid == low || nums[mid] > nums[mid-1]){
+                    return mid;
+                }else{
+                    high = mid - 1;
+                }
+            }else if(nums[mid] > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return new int[] {-1,-1};
+        if(nums[0]>target) return new int[] {-1,-1};
+        if(nums[nums.length-1]<target) return new int[] {-1,-1};
+        int firstIndex = binarySearchFirst(nums, target, 0, nums.length-1);
+        if(firstIndex == -1) return new int[] {-1,-1};
+        int lastIndex = binarySearchLast(nums, target, firstIndex, nums.length-1);
+        return new int [] {firstIndex,lastIndex};
+    }
+}
